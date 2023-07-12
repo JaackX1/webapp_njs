8ab2f778c2a89017e65f12e9c7215b6230198378
JaackX1 <nicolascabreramillacan@gmail.com>
2023-07-11 21:59:05 -0400

    change footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -82,7 +82,7 @@ const footer = () => {
           </a>
         </div>
         <p className="mt-8 text-base leading-6 text-center text-white">
-          © 2021 SomeCompany, Inc. All rights reserved.
+          © 2023 JaackX1, Inc. All rights reserved.
         </p>
       </div>
     </footer>
